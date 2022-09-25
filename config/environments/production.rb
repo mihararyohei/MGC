b0bc4ce9b09f68c54463839d800c2517dd721b9c
EC2 Default User <108612635+mihararyohei@users.noreply.github.com>
2022-09-25 13:50:51 +0000

    [update]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://13.231.127.5/' ]
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.url = "ws://13.231.127.5/cable"
+  config.action_cable.allowed_request_origins = ['http://13.231.127.5']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
