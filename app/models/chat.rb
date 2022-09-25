514e3bc926d0a526f7021d868325011a91713fb0
EC2 Default User <108612635+mihararyohei@users.noreply.github.com>
2022-09-25 08:15:58 +0000

    [update]

--- app/models/chat.rb
+++
@@ -1,5 +0,0 @@
-class Chat < ApplicationRecord
-  has_many :communities, dependent: :destroy
-  belongs_to :customer
-  belongs_to :community
-end
--- app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :customer
-  belongs_to :chat
-end
