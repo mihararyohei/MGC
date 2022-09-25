66cc1594f9ee4e784b7d1b6bdcc72fdfd0118547
EC2 Default User <108612635+mihararyohei@users.noreply.github.com>
2022-09-25 12:33:24 +0000

    [update]

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -9,27 +9,32 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(data)
     # Messageにデータを保存
-    Message.create(
+   message = Message.new(
       customer_id: current_customer.id,
       room_id: params['room_id'],
       content: data['message']
     )
-
+    message.save
     # 部分テンプレートをWebSocket経由で送り出す。
     #   render_messageで部分テンプレートに文字を埋め込みmessageとして送り出している。
-    ActionCable.server.broadcast("room_channel_#{params['room_id']}", { message: render_message(data) })
+    ActionCable.server.broadcast("room_channel_#{params['room_id']}", { message: render_message(message) })
+    # ActionCable.server.broadcast("room_channel_#{params['room_id']}", { message: 'test' })
   end
 
   private
 
-  def render_message(data)
-    renderer = ApplicationController.renderer.new(
-      http_host: ENV['HOST_URL'],
-      https: ENV['HTTPS']
-      )
-    return renderer.render(
-      partial: 'public/messages/message',
-      locals: { message: data, current_customer: current_customer }
-    )
+  def render_message(message)
+    ApplicationController.renderer.render(partial: 'public/messages/message', locals: { message: message, current_customer: current_customer })
+    # renderer = ApplicationController.renderer.new(
+    #   http_host: ENV['HOST_URL'],
+    #   https: ENV['HTTPS']
+    #   )
+    #   # byebug
+    # # return renderer.render(
+    # #   # partial: 'public/messages/message',
+    # #   # locals: { message: data, current_customer: current_customer }
+
+    # # )
+    # return render text: 'test'
   end
 end
